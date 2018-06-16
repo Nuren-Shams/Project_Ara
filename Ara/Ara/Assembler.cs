96f4eecfc967a37809e52f8934443c8f7deb48d1
Nuren_Shams <Nuren_Shams@Outlook.com>
2018-06-16 11:20:10 +0600

    Added the "Format" method to preprocess an instruction string.

---
+++ Ara/Ara/Assembler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Ara {
+    public static class Assembler {
+        private static readonly string CharacterSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789($)";
+        private static string Format(string Instruction) {
+            Instruction = Instruction.ToUpper();
+            StringBuilder InstructionBuilder = new StringBuilder(Instruction);
+            for(int Iterator=0; Iterator<InstructionBuilder.Length; Iterator++) {
+                if (!CharacterSet.Contains(InstructionBuilder[Iterator])) {
+                    InstructionBuilder[Iterator] = ' ';
+                }
+            }
+            Instruction = InstructionBuilder.ToString();
+            Instruction = Regex.Replace(Instruction, "\\s+", " ");
+            Instruction = Instruction.Trim();
+            return Instruction;
+        }
+    }
+}
---
+++ Ara/Ara/Source.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ara {
+    public static class Source {
+        public static void Main(string[] args) {
+            Console.WriteLine("Designed and Written by ____________");
+            Console.WriteLine("                        NUREN SHAMS/");
+            Console.WriteLine();
+            //Code start
+
+
+
+            //Code end
+            Console.Write("\nPress any key to exit. . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
